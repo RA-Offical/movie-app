29b7ce1426563865e44cfb482111d1c8cbde568f
Rashid Ameer <r.ameer.offical@gmail.com>
2023-07-22 23:39:15 +0500

    Finish some touches on alert

--- src/components/alert/alert.jsx
+++ src/components/alert/alert.jsx
@@ -2,7 +2,7 @@ import "./alert.css";
 
 const Alert = ({ generalClasses }) => {
 	return (
-		<div className={`${generalClasses} alert alert--success`} role="alert">
+		<div className={`${generalClasses} alert alert--danger`} role="alert">
 			<p className="alert__msg">Movie added to watchlist successfully</p>
 		</div>
 	);
