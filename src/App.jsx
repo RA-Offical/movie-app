dc33375166cc20ef6cf62cfdd9c82212d2441704
Rashid Ameer <r.ameer.offical@gmail.com>
2023-07-22 23:30:11 +0500

    . Finish generalize alert component . Finish Specific Fixed Alert component which is a wrapper over generalize alert component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Header, Hero, Movies } from "./containers";
+import { FixedAlert } from "./components";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
 			<Header />
 			<Hero />
 			<main>
+				<FixedAlert />
 				<Movies />
 			</main>
 		</div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Alert } from "./alert/alert";
+export { default as FixedAlert } from "./fixed-alert/fixed-alert";
 export { default as Loader } from "./loader/loader";
 export { default as Navbar } from "./navbar/navbar";
 export { default as Pagination } from "./pagination/pagination";
