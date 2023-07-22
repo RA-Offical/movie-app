b9ed1ea74eb164438decf1dc172a2ed61ae951d8
Rashid Ameer <r.ameer.offical@gmail.com>
2023-07-22 22:42:56 +0500

    - Install React Icons library - Finish Movies component - Finish Movie component. I added two specific button classes in movie compoenent because these buttons (add and remove movie button) belong to movie compoenent. - Finish ButtonIcon only component - Added generalize button icon only classes. (A button only with icon in it) - Added z-index on header-wrapper so that it always remain on top - Added generalize icon and icon-small classes

--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -4,7 +4,7 @@ import "./header.css";
 const Header = () => {
 	return (
 		<div className="fixed top-0 left-0 right-0 header-wrapper">
-			<header className="max-fw margin-c md-padx-1x pady-20x flex align-ic header">
+			<header className="max-fw mg-center md-padx-1x pady-20x flex align-ic header">
 				<a href="#" className="logo">
 					RMovies
 				</a>
--- src/containers/movies/movies.jsx
+++ src/containers/movies/movies.jsx
@@ -1,7 +1,20 @@
+import { Movie } from "../../components";
 import "./movies.css";
 
 const Movies = () => {
-	return <h1>Movies</h1>;
+	return (
+		<section className="movies-cont">
+			<div className="max-fw mg-center md-padx-1x md-pady-2x grid justify-btw movies">
+				<Movie />
+				<Movie />
+				<Movie />
+				<Movie />
+				<Movie />
+				<Movie />
+				<Movie />
+			</div>
+		</section>
+	);
 };
 
 export default Movies;
